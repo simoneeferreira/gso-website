29ad0f17f862c71ccb98d6982e21054357024599
simoneeferreira <simoneef1986@gmail.com>
2020-08-02 23:25:54 +0100

    second commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
-        if (scroll > 300) {
+        if (scroll > 50) {
           $(".blue").css("background" , "#EFF1F4");
           $(".logo-menu").css("visibility", "visible");
-          navbar.classList.remove('shadow');
-          navbar.classList.add('border-bottom');
+          navbar.classList.remove('border-bottom');
+          navbar.classList.add('shadow');
           navbar.classList.remove('navbar-dark');
           navbar.classList.add('navbar-light');
           
@@ -14,8 +14,8 @@ $(document).ready(function(){
         else{
             $(".blue").css("background" , "transparent");
             $(".logo-menu").css("visibility", "hidden");  
-            navbar.classList.remove('border-bottom');
-            navbar.classList.add('shadow');
+            navbar.classList.remove('shadow');
+            navbar.classList.add('border-bottom');
             navbar.classList.remove('navbar-light');
             navbar.classList.add('navbar-dark');
             	
